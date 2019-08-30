63386be9e550f4774504e7221bbbc6228e2f16c7
Sangram Reddy <reddy.horcrux@gmail.com>
2019-08-30 16:49:03 +0100

    Added delivery cost id integration

--- src/orders/createOrder.js
+++ src/orders/createOrder.js
@@ -16,10 +16,10 @@ module.exports.fun = async (event, context, callback) => {
     let customer_id = event.cognitoPoolClaims.sub
     let data = event.body
     let query = `
-        CALL orders.create_order(UUID_TO_BIN(?),?);
+        CALL orders.create_order(UUID_TO_BIN(?),?,?);
     `;
     console.log("Running query", query);
-    let results = await mysql.query(query, [ customer_id, data.delivery_address_id ])
+    let results = await mysql.query(query, [ customer_id, data.delivery_address_id, data.delivery_cost_id ])
     await mysql.end()
     return results
 }
